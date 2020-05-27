20fa9bc31515300d41ad91d99d158a7fd7e69a54
Johannes Steltzer <Johannes Steltzer johannes.steltzer@t-systems.com>
2020-05-27 14:45:51 +0200

    enable proxy for HttpClient

--- src/main/java/cmanager/network/ApacheHttp.java
+++ src/main/java/cmanager/network/ApacheHttp.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import org.apache.http.HttpHeaders;
+import org.apache.http.HttpHost;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -14,10 +15,21 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
 
 public class ApacheHttp {
 
-    private final CloseableHttpClient httpClient = HttpClients.createDefault();
+    private final CloseableHttpClient httpClient;
+    
+    public ApacheHttp() {
+        if (System.getProperty("https.proxyHost") != null) {
+            HttpHost httpsProxy = new HttpHost(System.getProperty("https.proxyHost"), Integer.parseInt(System.getProperty("https.proxyPort")));
+            DefaultProxyRoutePlanner routePlanner = new DefaultProxyRoutePlanner(httpsProxy);
+            httpClient = HttpClients.custom().setRoutePlanner(routePlanner).build();
+        } else {
+            httpClient = HttpClients.createDefault();
+        }
+    }
 
     public static class HttpResponse {
 
